8832ca325c46726df2d3832e8d45cbc79067a35f
User <User@DESKTOP-OCFGK81>
2021-12-09 13:41:57 +0530

    Demo Message

---
+++ src/main/java/SDETPROJECT26/SDETTY/App.java
@@ -0,0 +1,13 @@
+package SDETPROJECT26.SDETTY;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SDETPROJECT26/SDETTY/Demo.java
@@ -0,0 +1,13 @@
+package SDETPROJECT26.SDETTY;
+
+import org.testng.annotations.Test;
+
+
+
+public class Demo  {
+	@Test
+	public void demo() throws Throwable {
+		System.out.println("Demo Script");
+	}
+}
+	
